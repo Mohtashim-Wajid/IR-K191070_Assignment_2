da7fe0d8a4b3f70dac40587ce74a5eec969214ae
Mohtashim_Wajid <91398268+Mohtashim-Wajid@users.noreply.github.com>
2023-04-10 21:45:17 +0500

    completed

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
+
+import nltk 
+from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 import glob
 import os
 import natsort
 
-# corpus_path = './Dataset/'  # change to your directory and file extension
-# doc_files = natsorted(os.listdir(corpus_path))  # get a sorted list of file names in the directory
-
-# doc_list = []  # initialize an empty list to store the document texts
-
-# # iterate over the document files and read their contents into doc_list as nested lists
-# for file in doc_files:
-#     with open(os.path.join(corpus_path, file), 'r') as f:
-#         text = f.read()  # read the file contents as a string
-#         doc_list.append([text])  # add the text to a new sublist in the doc_list
-
-# # iterate over the doc_list and do something with each sublist
-
-
-# # for sublist in doc_list:
-# #     print(sublist)  # print the current sublist
 
 
 
+# -------------------- corpus Loading -------------------------
 
 corpus_path = './Dataset/*.txt'  # change to your directory and file extension
 doc_paths = natsort.natsorted(glob.glob(corpus_path))  # sort the file paths using natsorted
@@ -40,3 +28,33 @@ vectorizer = TfidfVectorizer()
 x = vectorizer.fit_transform(corpus_list)
 print(vectorizer.get_feature_names_out())
 print(x.shape)
+
+
+nltk.download('punkit')
+nltk.download('stopwords')
+
+stop_words = set(stopwords.words('english'))
+
+
+
+# ----------------- PREPROCESSING -----------------
+
+# --------------------- tokenization ------------------- 
+def get_tokenized_list(doc_text):
+    tokens = nltk.word_tokenize(doc_text)
+    return tokens
+
+
+def word_stemmmer(token_list):
+    ps = nltk.stem.PorterStemmer()
+    stemmed = []
+    for words in token_list:
+        stemmed.append(ps.stem(words))
+    return stemmed
+
+def remove_stopwords(doc_text):
+    cleaned_text = []
+    for words in doc_text:
+        if words not in stop_words:
+            cleaned_text.append(words)
+    return cleaned_text

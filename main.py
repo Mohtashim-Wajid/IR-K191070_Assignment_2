079d83b35c67134025961d68890ebcb7b9de223d
Mohtashim_Wajid <91398268+Mohtashim-Wajid@users.noreply.github.com>
2023-04-10 21:46:00 +0500

    completed

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 import nltk 
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 import glob
 import os
 import natsort
+import pandas as pd
 
 
 
@@ -24,14 +26,14 @@ for path in doc_paths:
         corpus_list.append(text)  # add the text to the corpus_list
 
 
-vectorizer = TfidfVectorizer()
-x = vectorizer.fit_transform(corpus_list)
-print(vectorizer.get_feature_names_out())
-print(x.shape)
+# vectorizer = TfidfVectorizer()
+# x = vectorizer.fit_transform(corpus_list)
+# print(vectorizer.get_feature_names_out())
+# print(x.shape)
 
 
-nltk.download('punkit')
-nltk.download('stopwords')
+# nltk.download('punkt')
+# nltk.download('stopwords')
 
 stop_words = set(stopwords.words('english'))
 
@@ -58,3 +60,47 @@ def remove_stopwords(doc_text):
         if words not in stop_words:
             cleaned_text.append(words)
     return cleaned_text
+
+
+cleaned_corpus = []
+for doc in corpus_list:
+    tokens = get_tokenized_list(doc)
+    doc_text = remove_stopwords(tokens)
+    doc_text = word_stemmmer(doc_text)
+    doc_text = ' '.join(doc_text)
+    cleaned_corpus.append(doc_text)
+# print(cleaned_corpus)
+
+
+
+vectorizerX = TfidfVectorizer()
+vectorizerX.fit(cleaned_corpus)
+doc_vector = vectorizerX.transform(cleaned_corpus)
+print(vectorizerX.get_feature_names_out())
+print(doc_vector.shape)
+
+dataframe = pd.DataFrame(doc_vector.toarray(), columns= vectorizerX.get_feature_names_out())
+# print(dataframe)
+
+
+# ------------------------------------ Query Processing -------------------------
+
+
+
+#                            modify the query here to search ....
+query = 'andrew pakistan'
+
+
+
+query = get_tokenized_list(query)
+query = remove_stopwords(query)
+q = []
+for w in word_stemmmer(query):
+    q.append(w)
+q = ' '.join(q)
+
+query_vector = vectorizerX.transform([q]) 
+cosineSimilarity = cosine_similarity(doc_vector,query_vector).flatten()
+related_docs_indices = cosineSimilarity.argsort()[:-10:-1]
+print(related_docs_indices)
+
